5e3bb27c42643dee132d496a65737beaca04fdb7
Anurag Singhal <anuragsinghal811@gmail.com>
2022-08-03 19:13:52 +0530

    Initial commit

---
+++ github.py
@@ -0,0 +1,8 @@
+# when a grp of 3-4 people works on a project, then they have to merge their files at one place
+# Ex: instead of share code on mails, dropbox, we use github,gitlab, bitbucket
+# GIT -  DISTRIBUTED VERSION CONTROL SYSTEM , how can we push opur project to github repository
+# u can think repository as a folder- where u can put all ur files , online storage like g-drive & dropbox
+# To save a file: click on commit file
+# files ki history ko save krke rkhta hai git,
+
+
